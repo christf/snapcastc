dea16f3d71140eec8a3b8a1224afb91b0d7ec5fc
Christof Schulze <christof.schulze@gmx.net>
2019-01-20 23:08:03 +0100

    Alsaplayer: close fd and rework stretching

--- src/alsaplayer.c
+++ src/alsaplayer.c
@@ -5,7 +5,8 @@
 #include "util.h"
 
 #include <alsa/asoundlib.h>
-#include <soxr.h>
+//#include <soxr.h>
+// #include <rubberband/rubberband-c.h>
 #include <stdio.h>
 
 #include "timespec.h"
@@ -14,32 +15,87 @@
 #define PERIOD_TIME 30000
 
 // TODO: adjust speed without changing the pitch
+bool resample(pcmChunk *chunk, double factor) {
+	uint16_t inframes = chunk->size / chunk->channels / chunk->frame_size;
+	uint16_t outframes = chunk->size / chunk->channels / chunk->frame_size * factor;
+	if (inframes == outframes || chunk->play_at.tv_sec == 0) {
+		// do not call sox when resampling would not have an effect
+		log_error("skipping resample\n");
+		return false;
+	}
+	return true;
+}
+void adjust_speed(pcmChunk *chunk, char *out, double factor) {
+
+	if (!resample(chunk, factor)) {
+		memcpy(out, chunk->data, chunk->size);
+		return;
+	}
+
+	uint16_t inframes = chunk->size / chunk->channels / chunk->frame_size;
+	uint16_t outframes = chunk->size / chunk->channels / chunk->frame_size * factor;
+
+	if (factor <= 1)
+		memcpy(out, chunk->data, outframes * chunk->channels * chunk->frame_size);
+	else if (factor > 1) {
+		memcpy(&out[chunk->channels*chunk->frame_size], chunk->data, chunk->size);
+		memcpy(out, chunk->data, chunk->frame_size * chunk->channels);
+	}
+
+	uint16_t olen =  chunk->size / chunk->channels / chunk->frame_size * factor;
+	log_error("adjusted frame with len: %d to olen: %d\n", chunk->size, olen);
+	chunk->size =  chunk->size / chunk->channels / chunk->frame_size * factor;
+}
+/*
+void adjust_speed(pcmChunk *chunk, char *out, double factor) {
+
+	if (!resample(chunk, factor)) {
+		memcpy(out, chunk->data, chunk->size);
+		return;
+	}
+
+	RubberBandState rbs = rubberband_new(chunk->samples,
+			chunk->channels, RubberBandOptionProcessRealTime,
+			factor,
+			1);
+	uint16_t inframes = chunk->size / chunk->channels / chunk->frame_size;
+	uint16_t outframes = chunk->size / chunk->channels / chunk->frame_size * factor;
+	
+	rubberband_set_max_process_size(rbs, inframes);
+	unsigned int required =  rubberband_get_samples_required(rbs);
+	log_error("required samples: %d, have: %d\n", required, inframes);
+	rubberband_set_max_process_size(rbs, inframes);
+	rubberband_process(rbs, (const float *const *)chunk->data, inframes, 0);
+	int  nb_samples = rubberband_available(rbs);
+	rubberband_retrieve(rbs, (float* *const)out, outframes);
+
+//	log_error("len: %d, olen: %d odone: %d sox-error: %d\n", chunk->size, olen, odone, error);
+	chunk->size = outframes * chunk->channels * chunk->frame_size;
+}
+*/
+/*
 void adjust_speed(pcmChunk *chunk, char *out, double factor) {
 	double orate = chunk->samples * factor;
 	size_t olen = (size_t)(chunk->size * orate / chunk->samples + .5);
 	size_t odone;
 
-	uint16_t inframes = chunk->size / chunk->channels / chunk->frame_size;
-	uint16_t outframes = chunk->size / chunk->channels / chunk->frame_size * factor;
-	if (inframes == outframes || chunk->play_at.tv_sec == 0) {
-		// do not call sox for performance reasons when resampling would not have an effect
-		log_error("skipping sox\n");
+	if (!resample(chunk, factor)) {
 		memcpy(out, chunk->data, chunk->size);
 		return;
 	}
 
-	soxr_quality_spec_t quality_spec = soxr_quality_spec(SOXR_VHQ, 0);
+	soxr_quality_spec_t quality_spec = soxr_quality_spec(SOXR_MQ, 0);
 	soxr_io_spec_t io_spec = soxr_io_spec(SOXR_INT16_I, SOXR_INT16_I);  // TODO this should not be hard-coded.
 
-	soxr_error_t error = soxr_oneshot(chunk->samples, orate, chunk->channels,				/* Rates and # of chans. */
-					  chunk->data, chunk->size / chunk->channels / chunk->frame_size, NULL, /* Input. */
-					  out, olen, &odone,							/* Output. */
-					  &io_spec, &quality_spec, NULL);					/* Default configuration.*/
+	soxr_error_t error = soxr_oneshot(chunk->samples, orate, chunk->channels,				//Rates and # of chans.
+					  chunk->data, chunk->size / chunk->channels / chunk->frame_size, NULL, // Input.
+					  out, olen, &odone,							// Output.
+					  &io_spec, &quality_spec, NULL);					// Default configuration.
 
 	log_error("len: %d, olen: %d odone: %d sox-error: %d\n", chunk->size, olen, odone, error);
 	chunk->size = olen;
 }
-
+*/
 int getchunk(char *buf, int buffsize, size_t delay_frames) {
 	const double adjustment = 0.01;
 	double factor = 1;
@@ -68,11 +124,11 @@ int getchunk(char *buf, int buffsize, size_t delay_frames) {
 		} else {
 			snapctx.alsaplayer_ctx.playing = true;
 			snapctx.alsaplayer_ctx.empty_chunks_in_row = 0;
+			reschedule_task(&snapctx.taskqueue_ctx, snapctx.alsaplayer_ctx.close_task, (1.2 * snapctx.bufferms) / 1000 , (int)(1.2 * snapctx.bufferms) % 1000);
 		}
 	} else
 		get_emptychunk(&p);
 
-	// TODO: should we adjust the factor based on how much the timing is off for faster sync on large buffers?
 	if (!is_near) {
 		factor = 1 - adjustment * tdiff.sign;
 		bool not_even_close = (tdiff.time.tv_sec == 0 && tdiff.time.tv_nsec < not_even_close_ms * 1000000L);
@@ -125,6 +181,11 @@ void alsaplayer_handle(alsaplayer_ctx *ctx) {
 	log_verbose("PCM delay frames: %d\n", delayp);
 }
 
+void alsaplayer_uninit_task(void *d) {
+	log_error("UNITIALIZING ALSA!!!!!!     !!!!!!\n\n\n\n");
+	alsaplayer_uninit(&snapctx.alsaplayer_ctx);
+}
+
 void alsaplayer_uninit(alsaplayer_ctx *ctx) {
 	if (!ctx->initialized)
 		return;
@@ -132,6 +193,19 @@ void alsaplayer_uninit(alsaplayer_ctx *ctx) {
 	snd_pcm_close(ctx->pcm_handle);
 	ctx->initialized = ctx->playing = false;
 	free(ctx->ufds);
+
+	for (int i = 0; i < ctx->pollfd_count; i++) {
+		log_error("size: %d %d %d\n", i, sizeof(*(ctx->main_poll_fd)) , sizeof(struct pollfd)) ;
+		ctx->main_poll_fd[i].fd = - (ctx->main_poll_fd[i]).fd;
+	}
+}
+
+void init_alsafd(alsaplayer_ctx *ctx) {
+	for (int i = 0; i < ctx->pollfd_count; i++) {
+		struct pollfd *pfd = &snapctx.alsaplayer_ctx.ufds[i];
+		ctx->main_poll_fd[i].fd = pfd->fd;
+		ctx->main_poll_fd[i].events = POLLIN;
+	}
 }
 
 /*
@@ -177,6 +251,8 @@ void alsaplayer_init(alsaplayer_ctx *ctx) {
 	unsigned int pcm, tmp;
 	int err;
 
+	ctx->close_task = post_task(&snapctx.taskqueue_ctx, (snapctx.bufferms * 1.2 ) / 1000 , (int)(snapctx.bufferms * 1.2) % 1000, alsaplayer_uninit_task, NULL, NULL);
+
 	ctx->empty_chunks_in_row = 0;
 	ctx->playing = false;
 
@@ -210,7 +286,7 @@ void alsaplayer_init(alsaplayer_ctx *ctx) {
 		snd_pcm_format = SND_PCM_FORMAT_S32_LE;
 	else
 		exit_error("unsupported format\n");
-
+	
 	if ((pcm = snd_pcm_hw_params_set_format(ctx->pcm_handle, ctx->params, snd_pcm_format)) < 0)
 		log_error("ERROR: Can't set format. %s\n", snd_strerror(pcm));
 
